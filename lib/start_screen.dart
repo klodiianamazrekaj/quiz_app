5f48ef6fc31416ba336647d21bccd3faff10f975
klodiianamazrekaj <102175182+klodiianamazrekaj@users.noreply.github.com>
2023-11-02 00:32:24 +0100

    added a start screen

---
+++ lib/start_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(221, 37, 34, 34),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 50),
+          FilledButton.icon(
+                  onPressed: () {},
+                  style: FilledButton.styleFrom(
+                    foregroundColor: const Color.fromARGB(221, 37, 34, 34),
+                    backgroundColor: const Color.fromARGB(255, 232, 206, 129),
+                  ),
+                  label: const Text('Start Quiz'),
+                  icon: const Icon(Icons.arrow_right_alt_sharp),
+                ),
+        ],
+      ),
+    );
+  }
+}
