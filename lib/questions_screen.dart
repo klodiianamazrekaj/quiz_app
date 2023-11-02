a78c159b6059a7f399b59cbc47bb23106f2deb07
klodiianamazrekaj <102175182+klodiianamazrekaj@users.noreply.github.com>
2023-11-02 01:21:16 +0100

    minor changes

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
   @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
